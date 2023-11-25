2fa54e6d324c5da657ebefec7b8f228a702bea6b
Javier A. Roque Sañudo <javicaib.mail@gmail.com>
2023-11-25 17:45:19 +0100

    exc 1.7

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -6,21 +6,28 @@ function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const handleClick = (setState)=>{
-    setState((prev)=>{
-      return prev += 1  
+  const handleClick = (setState) => {
+    setState((prev) => {
+      return prev += 1
     })
   }
+  const total = good + neutral + bad
+  const avg = (good - bad)/total
+  const positve = (good*100)/total
+  
   return (
     <>
-     <h1>Give us feedback</h1>
-     <button onClick={()=>handleClick(setGood)}>Good</button>
-     <button  onClick={()=>handleClick(setNeutral)}>Neutral</button>
-     <button  onClick={()=>handleClick(setBad)}>Bad</button>
-     <h2>Statistics</h2>
-     <p>Good : {good}</p>
-     <p>Neutal :{neutral}</p>
-     <p>Bad : {bad}</p>
+      <h1>Give us feedback</h1>
+      <button onClick={() => handleClick(setGood)}>Good</button>
+      <button onClick={() => handleClick(setNeutral)}>Neutral</button>
+      <button onClick={() => handleClick(setBad)}>Bad</button>
+      <h2>Statistics</h2>
+      <p>Good : {good}</p>
+      <p>Neutal :{neutral}</p>
+      <p>Bad : {bad}</p>
+      <p>All : {total}</p>
+      <p>Average: {avg}</p>
+      <p>Positve: {positve} </p>
     </>
   )
 }
