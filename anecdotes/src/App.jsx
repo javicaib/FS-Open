e1cac12a229ee433acb616498b4e1dfa159c10ae
Javier A. Roque Sañudo <javicaib.mail@gmail.com>
2023-11-25 18:53:31 +0100

    exc 1.14

--- anecdotes/src/App.jsx
+++ anecdotes/src/App.jsx
@@ -2,9 +2,6 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [selected, setSelected] = useState(0)
-  const [votes,setVotes] = useState({ 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 })
-
   const anecdotes = [
     'If it hurts, do it more often',
     'Adding manpower to a late software project makes it later!',
@@ -13,6 +10,12 @@ function App() {
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
   ]
+
+  const [selected, setSelected] = useState(0)
+  const initialVote = new Array(anecdotes.length).fill(0)
+  const [votes,setVotes] = useState(initialVote)
+  const [max,setMax]=  useState(0)
+
   const handleClick = () => {
     setSelected(Math.floor(Math.random() * anecdotes.length))
   }
@@ -22,7 +25,11 @@ function App() {
       ...prev,
       [selected]: prev[selected] + 1
     }
-   })
+   }
+   )
+   if (votes[selected] > votes[max]) {
+    setMax(selected)
+  }
   }
   return (
     <>
@@ -35,7 +42,12 @@ function App() {
         <button onClick={handleVote}>Vote</button>
         <button onClick={handleClick}>Next Anecdote</button>
       </div>
+      <h2>Anecdote whit most votes</h2>
+      {anecdotes[max]}
 
+      <div>
+        <span>has {votes[max]} votes</span>
+      </div>
     </>
   )
 }
