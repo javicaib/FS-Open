c5f42424d7b1465e8b7b5a1506c4537131c30b4e
Javier A. Roque Sañudo <javicaib.mail@gmail.com>
2023-11-25 16:04:57 +0100

    exc 1.6

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,33 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const handleClick = (setState)=>{
+    setState((prev)=>{
+      return prev += 1
+    })
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1>Give us feedback</h1>
+     <button onClick={()=>handleClick(setGood)}>Good</button>
+     <button  onClick={()=>handleClick(setNeutral)}>Neutral</button>
+     <button  onClick={()=>handleClick(setBad)}>Bad</button>
+     <h2>Statistics</h2>
+     <p>Good : {good}</p>
+     <p>Neutal :{neutral}</p>
+     <p>Bad : {bad}</p>
     </>
   )
 }
