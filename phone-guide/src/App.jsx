5112d271df81d0505b4697e8ac6ca5f5de5e0d4d
Javier A. Roque <javicaib.mail@gmail.com>
2023-11-28 09:24:30 -0500

    exc 2.7

--- phone-guide/src/App.jsx
+++ phone-guide/src/App.jsx
@@ -1,34 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+  const handleSubmit = (event)=>{
+    event.preventDefault()
+    
+    // Get Input Value
+    const { elements } = event.currentTarget;
+    const name = elements.namedItem("fullname");
+    const isInputName = name instanceof HTMLInputElement;
+    if (!isInputName || name === null) return;
 
+    if(persons.filter(person => person.name === name.value).length !== 0) {
+      alert(`${name.value} is already added to phonebook`)
+      return
+    }
+    setNewName(name.value)
+    name.value = ''
+  }
+  useEffect(()=>{
+    const newPerson = {
+      name : newName
+    }
+    setPersons((prev)=> {
+      return [...prev,newPerson]
+    })
+    setNewName('')
+  },[newName])
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          Name: <input name='fullname' />
+        </div>
+        <div>
+          <button type="submit">Add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person=>{
+        return (
+          <p key={crypto.randomUUID()}>{person.name}</p>
+        )
+      })}
+      
+    </div>
   )
 }
 
