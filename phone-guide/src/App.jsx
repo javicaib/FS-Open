91953ff585963db7373e05808e3e7b8252c582e5
Javier A. Roque <javicaib.mail@gmail.com>
2023-11-28 09:51:18 -0500

    2.8

--- phone-guide/src/App.jsx
+++ phone-guide/src/App.jsx
@@ -1,42 +1,65 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 
 function App() {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      phone : 564412333
+  }
   ]) 
-  const [ newName, setNewName ] = useState('')
   const handleSubmit = (event)=>{
     event.preventDefault()
-    
-    // Get Input Value
+
+    // Get Input from Form event
     const { elements } = event.currentTarget;
+    // Get Input Name Value
     const name = elements.namedItem("fullname");
     const isInputName = name instanceof HTMLInputElement;
+    
+    // Validate if is null
     if (!isInputName || name === null) return;
+    
+    // Get Input Phone Value
+    const phone = elements.namedItem("phone");
+    const isInputPhone = name instanceof HTMLInputElement;
+    
+    // Validate if is null
+    if (!isInputPhone || name === null) return;
 
+
+    // Validates
     if(persons.filter(person => person.name === name.value).length !== 0) {
       alert(`${name.value} is already added to phonebook`)
       return
     }
-    setNewName(name.value)
-    name.value = ''
-  }
-  useEffect(()=>{
-    const newPerson = {
-      name : newName
+    if(persons.filter(person => person.phone === phone.value).length !== 0) {
+      alert(`${phone.value} is already added to phonebook`)
+      return
     }
+    const newPerson = {
+      name : name.value,
+      phone : phone.value
+    }
+  
     setPersons((prev)=> {
       return [...prev,newPerson]
     })
-    setNewName('')
-  },[newName])
+
+    name.value = ''
+    phone.value = ''
+    
+  }
+  
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
+      <div>
+          Name: <input required name='fullname' />
+        </div>
         <div>
-          Name: <input name='fullname' />
+          Phone: <input required name='phone' />
         </div>
         <div>
           <button type="submit">Add</button>
@@ -45,7 +68,7 @@ function App() {
       <h2>Numbers</h2>
       {persons.map(person=>{
         return (
-          <p key={crypto.randomUUID()}>{person.name}</p>
+          <p key={crypto.randomUUID()}>{person.name} - {person.phone}</p>
         )
       })}
       
